5b6b8d991227b9333fd2143f2b5da35cac29f103
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2026-05-20 21:54:12 +0600

    setup PostgreSQL tables with check constraints and neon serverless integration

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,33 @@
 import { neon } from "@neondatabase/serverless";
 import config from "../config";
 
+export const sql = neon(config.database_url);
 
-export const sql= neon(config.database_url)
+export const initDB = async () => {
+  await sql`
+    CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(75) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role VARCHAR(20) NOT NULL DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )
+    `;
 
-export const initDB = async()=>{
-    console.log("Database Connected Successfully!")
-}
+  await sql`
+    CREATE TABLE IF NOT EXISTS issues (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    description TEXT NOT NULL,
+    type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+    status VARCHAR(20) NOT NULL DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+    reporter_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+    )
+    `;
+
+  console.log("Database Connected Successfully!");
+};
